d4de7fd5a38f446805b59ab54b26f53d7979c322
Aryan Prince <aryanprincesanty@icloud.com>
2023-11-14 02:24:28 +0000

    💄 Added themes with `next-theme`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/ui/button";
+import MainNav from "@/components/main-nav";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
   return (
-    <main>
-      <p>Welcome to Sahara</p>
-      <Button>Click me</Button>
+    <main className="flex min-h-screen flex-col justify-between bg-background text-foreground">
+      <header>
+        <MainNav />
+      </header>
+      <h1 className="text-6xl font-bold tracking-tight">Welcome to Sahara</h1>
+      <footer className="mb-2 flex justify-center">
+        <ThemeToggle />
+      </footer>
     </main>
   );
 }
---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export function ThemeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="flex items-center gap-2">
+          <span>Toggle theme</span>
+          <div>
+            <Sun size={18} className="block dark:hidden" />
+            <Moon size={18} className="hidden dark:block" />
+          </div>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
